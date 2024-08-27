3d7aebecead6575a20d696b6a1aa247848a17ce3
ashmicmck1 <ashmicmck1@gmail.com>
2024-08-26 19:50:03 -0500

    Adding all required fields

---
+++ app/src/main/java/com/amckinn9/namebadge/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.amckinn9.namebadge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.amckinn9.namebadge.ui.theme.NameBadgeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NameBadgeTheme {
+                NameBadgeInfo()
+            }
+        }
+    }
+}
+
+@Composable
+fun NameBadgeInfo() {
+    Column {
+        Text(text = "Ashleigh")
+        Text(text = "McKinney")
+        Image(
+            painter = painterResource(R.drawable.badge_photo),
+            contentDescription = "Badge picture"
+        )
+        Text(text = "COMPUTER SCIENCE")
+        Text(text = "Senior")
+        Row {
+            Text(text = "GitHub-")
+            Text(text = "LinkedIn")
+            Text(text = "Email")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NameBadgePreview() {
+    NameBadgeTheme {
+        NameBadgeInfo()
+    }
+}
